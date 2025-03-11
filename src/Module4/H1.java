d749ef5636ce00e2bc34a29e9e04d9e9ed21a96d
nasrlol <nsrddyn@gmail.com>
2025-03-11 21:11:38 +0100

    Module 4 H1 begonnen

--- src/Module4/H1.java
+++ src/Module4/H1.java
@@ -1,4 +1,21 @@
 package Module4;
 
+abstract class dier
+{
+    private String geboorteDatum;
+    public String getGeboorteDatum()
+    {
+        return this.geboorteDatum;
+    }
+    public void setGeboorteDatum(String geboorteDatum)
+    {
+        this.geboorteDatum = geboorteDatum;
+    }
+}
+
 public class H1 {
+   public static void main(String[] args)
+   {
+
+   }
 }
